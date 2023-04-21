fcf9096d75c69782a0e8258859f752c53614a0e4
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2023-04-21 22:51:46 +0900

    [Bronze V] Title: A-B, Time: 60 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,8 @@
+using System;
+class a{
+    static void Main(){
+        string[] b = Console.ReadLine().Split();
+        int[] a = {int.Parse(b[0]), int.Parse(b[1])};
+        Console.WriteLine(a[0] - a[1]);
+    }
+}
