69bb2ea8a7dab374753370fc16bd14e5bd93852a
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2023-05-24 18:25:47 +0900

    [Bronze V] Title: 고양이, Time: 52 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,11 @@
+using System;
+class A
+{
+    static void Main()
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
