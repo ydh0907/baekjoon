b81d6c27bc2dbda384eb8ed908594248c0757288
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2023-08-28 17:58:49 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    int i;
+    for(i = 0; money >= 5500; money -= 5500, i++){
+        
+    }
+    answer.push_back(i);
+    answer.push_back(money);
+    return answer;
+}
