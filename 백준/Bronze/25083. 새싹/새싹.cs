4735aa3d6fab50f91e4ded05939060b1187613be
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2023-09-11 00:05:44 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,13 @@
+using System;
+class A
+{
+    static void Main()
+    {
+        Console.WriteLine("         ,r'\"7");
+        Console.WriteLine("r`-_   ,'  ,/");
+        Console.WriteLine(" \\. \". L_r'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+    }
+}
