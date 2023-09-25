a53815d62eaf1f56026b84c1ef218eda8efa0cb0
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2023-09-25 18:35:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.01 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.cpp
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -1,22 +1,18 @@
-#include <string>
+#include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-long long solution(string numbers) {
-    long long answer = 0;
-    vector<string> nums = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+long long solution(string str) {
+    vector<string> a = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
     string temp = "";
-    for (long long i = numbers.size() - 1, k = 1; i >= 0; i--) {
-        string a = "";
-        a += numbers[i];
-        temp.insert(0, a);
+    long long answer = 0;
+    for (int i = 0; i < str.size(); i++) {
+        temp += str[i];
         for (int j = 0; j < 10; j++) {
-            if (nums[j] == temp) {
-                answer += j * k;
+            if (temp == a[j]) {
                 temp = "";
-                k *= 10;
+                answer *= 10;
+                answer += j;
             }
         }
     }
