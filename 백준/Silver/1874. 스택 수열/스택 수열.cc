f7fea4af85812c7dc9dfda51fdecb9ea7c4e300e
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2024-04-16 15:28:59 +0900

    [Silver II] Title: 스택 수열, Time: 52 ms, Memory: 2780 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;
+
+	string out;
+	stack<int> s;
+	int next = 1;
+	int last = 0;
+
+	for (int i = 0; i < num; i++) {
+		int input;
+		cin >> input;
+
+		while (s.empty() || s.top() < input) {
+			s.push(next++);
+			out += "+\n";
+		}
+
+		if (input < s.top()) {
+			cout << "NO";
+			return 0;
+		}
+
+		s.pop();
+		out += "-\n";
+	}
+
+	cout << out;
+}
