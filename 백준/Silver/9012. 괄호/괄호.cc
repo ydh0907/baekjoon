e434853da98a5e11608ccead7a16e05b0d0b6c28
ydh0907 <123716061+ydh0907@users.noreply.github.com>
2024-04-16 14:51:01 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main() {
+	int count;
+	cin >> count;
+
+	for (int i = 0; i < count; i++) {
+		int flag = 0;
+
+		string s;
+		cin >> s;
+
+		for (char c : s) {
+			if (c == '(') ++flag;
+			else --flag;
+			if (flag < 0)
+				flag = -1000;
+		}
+
+		if (flag == 0) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+}
